05e960cacc9a56cc16f1240592865b2a924dbfce
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2025-07-21 16:59:06 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './Portfolio';  // 컴포넌트 경로
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
