fbebaf8cfd1769551a88ba170a7f1c94cc538cf9
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2025-07-22 10:22:05 +0900

    up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your existing HTML content will go here */}
+      {/* For now, a simple message */}
+      <h1>Welcome to YuMin-Ahn's Portfolio</h1>
+      <p>This is a placeholder for your React application.</p>
+    </div>
+  );
+}
+
+export default App;
